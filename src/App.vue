5bd10c7efb93d9f6807a6637d3d4464752e5dbd4
Darel Johnson <dajohnson@hodgesmace.com>
2018-03-23 15:11:05 -0400

    Removed unnessary items from toolbar and changed theme colors

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
   <v-app>
     <v-navigation-drawer
-      persistent
+      temporary
       :mini-variant="miniVariant"
       :clipped="clipped"
-      v-model="drawer"
+      v-model="sideNav"
       enable-resize-watcher
       fixed
       app
@@ -27,42 +27,15 @@
     <v-toolbar
       app
       :clipped-left="clipped"
+      class="primary"
+      dark
     >
-      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
-      <v-btn icon @click.stop="miniVariant = !miniVariant">
-        <v-icon v-html="miniVariant ? 'chevron_right' : 'chevron_left'"></v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="clipped = !clipped">
-        <v-icon>web</v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="fixed = !fixed">
-        <v-icon>remove</v-icon>
-      </v-btn>
+      <v-toolbar-side-icon @click.stop="sideNav = !sideNav"></v-toolbar-side-icon>
       <v-toolbar-title v-text="title"></v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn icon @click.stop="rightDrawer = !rightDrawer">
-        <v-icon>menu</v-icon>
-      </v-btn>
     </v-toolbar>
     <v-content>
       <router-view/>
     </v-content>
-    <v-navigation-drawer
-      temporary
-      :right="right"
-      v-model="rightDrawer"
-      fixed
-      app
-    >
-      <v-list>
-        <v-list-tile @click="right = !right">
-          <v-list-tile-action>
-            <v-icon>compare_arrows</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-title>Switch drawer (click me)</v-list-tile-title>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
     <v-footer :fixed="fixed" app>
       <span>&copy; 2017</span>
     </v-footer>
@@ -74,16 +47,14 @@ export default {
   data () {
     return {
       clipped: false,
-      drawer: true,
+      sideNav: false,
       fixed: false,
       items: [{
         icon: 'bubble_chart',
         title: 'Inspire'
       }],
       miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js'
+      title: 'DevMeetup'
     }
   },
   name: 'App'
--- src/main.js
+++ src/main.js
@@ -28,13 +28,13 @@ Vue.use(Vuetify, {
     transitions
   },
   theme: {
-    primary: '#ee44aa',
-    secondary: '#424242',
-    accent: '#82B1FF',
-    error: '#FF5252',
-    info: '#2196F3',
-    success: '#4CAF50',
-    warning: '#FFC107'
+    primary: '#f44336',
+    secondary: '#e57373',
+    accent: '#9c27b0',
+    error: '#f44336',
+    info: '#2196f3',
+    success: '#4caf50',
+    warning: '#ffeb3b'
   }
 })
 
